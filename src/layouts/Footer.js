147bfee030ec2aa028b8cd4998f6ed7d7ab3678f
TrunPT <phtrun23506@gmail.com>
2025-11-12 02:39:26 +0000

    CK

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -5,7 +5,10 @@ const Footer = () => {
         <div class="container_main">
           <div class="logo_footer">
             <a href="">
-              <img src="" alt="Logo" />
+              <img
+                src="https://i.pinimg.com/736x/f3/03/a3/f303a31db7a0e9237f169f8397ee01aa.jpg"
+                alt="Logo"
+              />
             </a>
           </div>
           <div class="content_footer"></div>
